42ad41666cf3b7b421729ffab575ffc755cb6c92
Eduardo Sanz García <esanzgar@gmail.com>
2018-05-09 11:16:05 +0100

    test: simplified not working example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,16 @@
 import { Component } from '@angular/core';
-import {
-    WorkflowFactory,
-    WorkflowModel,
-} from 'cwlts/models';
-import {
-    Workflow
-} from 'cwlts/mappings/v1.0/Workflow';
+import { WorkflowFactory } from 'cwlts/models';
+import * as OneStepWF from 'cwlts/tests/apps/one-step-wf';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  private _cwlModel: WorkflowModel | null = null;
+    title = 'app';
 
-  public readCWL(){
-      this._cwlModel = WorkflowFactory.from({} as Workflow);
-  }
+    constructor() {
+        const wf = WorkflowFactory.from(OneStepWF.default);
+    }
 }
